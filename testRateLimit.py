87a6dd7ae84a8fd112bf2ddd68524dcf9e324028
Maciej <59297860+jaszczomp71@users.noreply.github.com>
2023-05-11 17:19:53 +0200

    Add files via upload

---
+++ testRateLimit.py
@@ -0,0 +1,21 @@
+import requests
+import time
+
+# Ustawienia testu
+url = 'https://honda.sit.sandis.io/api/security/login'  # adres URL endpoint
+liczba_zadan = 30  # liczba zadań do wykonania
+przerwa = 0.5  # czas oczekiwania między zadaniami w sekundach
+
+for i in range(liczba_zadan):
+    
+    response = requests.get(url)
+    
+    if response.status_code == 405:
+        print(f'Zadanie {i+1}: Otrzymano kod 405 (błąd danych logowania)')
+    
+    elif response.status_code == 503:
+        print(f'Zadanie: {i+1}: Otrzymano kod 503 (przekroczony limit zapytani/1min.)')
+    
+    else:    
+         print(f'Zadanie {i+1}: Otrzymano kod {response.status_code}')
+    time.sleep(przerwa)
